2c2db84fb823a37a58ec82824e5f4678067872f5
prajwalsangappam-spec <prajwalsangappam@gmail.com>
2025-11-15 09:19:02 +0530

    Fix indentation and error handling in math.py

--- sajeed math.py
+++ sajeed math.py
@@ -1,9 +1,9 @@
 # math.py
- try:
-a = float(input("enter first number :"))
-b = float(input("enter second number :"))
-
-print(f"Addition :{a+b}")
-print(f"substraction :{a-b}")
+try:
+  a = float(input("enter first number :"))
+  b = float(input("enter second number :"))
+  
+  print(f"Addition :{a+b}")
+  print(f"substraction :{a-b}")
 except ValueError:
-print("please enter valid number.")
+  print("please enter valid number.")
